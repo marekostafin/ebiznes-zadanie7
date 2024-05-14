3c25a54b172ca830c38d8b82808955cbb61201fb
Marek Ostafin <marek.ostafin16@gmail.com>
2024-05-14 22:46:11 +0200

    Zadanie7b

--- server.go
+++ server.go
@@ -9,6 +9,8 @@ import (
 
 var products = []string{"Ciasto", "Placek", "Tort"}
 
+const product_not_found_msg = "Product not found"
+
 func createProduct(c echo.Context) error {
 	product := c.FormValue("product")
 	products = append(products, product)
@@ -27,7 +29,7 @@ func getProduct(c echo.Context) error {
 	}
 	product := products[productId]
 	if product == "" {
-		return c.JSON(http.StatusNotFound, "Product not found")
+		return c.JSON(http.StatusNotFound, product_not_found_msg)
 	}
 	return c.JSON(http.StatusOK, product)
 }
@@ -40,7 +42,7 @@ func updateProduct(c echo.Context) error {
 	}
 	product := c.FormValue("product")
 	if products[productId] == "" {
-		return c.JSON(http.StatusNotFound, "Product not found")
+		return c.JSON(http.StatusNotFound, product_not_found_msg)
 	}
 	products[productId] = product
 	return c.JSON(http.StatusOK, product)
@@ -53,22 +55,23 @@ func deleteProduct(c echo.Context) error {
 		return err
 	}
 	if products[productId] == "" {
-		return c.JSON(http.StatusNotFound, "Product not found")
+		return c.JSON(http.StatusNotFound, product_not_found_msg)
 	}
 	products = append(products[:productId], products[productId+1:]...)
 	return c.NoContent(http.StatusNoContent)
 }
 
 func main() {
+	const products_id_url = "/products/:id"
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 	e.POST("/products", createProduct)
 	e.GET("/products", getProducts)
-	e.GET("/products/:id", getProduct)
-	e.PUT("/products/:id", updateProduct)
-	e.DELETE("/products/:id", deleteProduct)
+	e.GET(products_id_url, getProduct)
+	e.PUT(products_id_url, updateProduct)
+	e.DELETE(products_id_url, deleteProduct)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
